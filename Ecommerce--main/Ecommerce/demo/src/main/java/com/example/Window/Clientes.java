f7fe1a75e5b856a755d7c6b5770000faba39c862
HenryAllenx <131827087+HenryAllenx@users.noreply.github.com>
2023-10-05 06:17:47 -0300

    Update Clientes.java

--- Ecommerce--main/Ecommerce/demo/src/main/java/com/example/Window/Clientes.java
+++ Ecommerce--main/Ecommerce/demo/src/main/java/com/example/Window/Clientes.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
+import com.example.Json.Product;
+
 public class Clientes implements ActionListener {
     final JFrame newFrame = new JFrame("O que desejas fazer?");
     JButton addcliente = new JButton("Adicionar Cliente: ");
@@ -51,7 +53,50 @@ public class Clientes implements ActionListener {
     @Override
     public void actionPerformed(java.awt.event.ActionEvent e) {
         if (e.getSource() == addcliente) {
-        
+             final JFrame opcoes = new JFrame("Criar novo Cliente");
+            opcoes.setLayout(null);
+            opcoes.setVisible(true);
+            opcoes.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            opcoes.setSize(600, 400);
+    
+        final JTextField nome = new JTextField("Digite o nome do Cliente");
+            nome.setBounds(10, 10, 200, 30);
+            opcoes.add(nome);
+            
+    
+        final JTextField cpf = new JTextField("Digite o cpf do Cliente");
+            cpf.setBounds(10, 50, 200, 30);
+            opcoes.add(cpf);
+    
+        final JTextField sexo = new JTextField("Digite o sexo do Cliente");
+            sexo.setBounds(10, 90, 200, 30);
+            opcoes.add(sexo);
+    
+        final JTextField saldo = new JTextField("Digite o Saldo do Cliente");
+            saldo.setBounds(10, 130, 200, 30);
+            opcoes.add(saldo);
+
+    
+            JButton confirmar = new JButton("Confirmar");
+            confirmar.setBounds(10, 170, 100, 30);
+            opcoes.add(confirmar);
+
+            confirmar.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e){
+                    Product.adicionarProduto(null);
+
+                    opcoes.setVisible(false);
+                }
+            });
+        }
+
+        if (e.getSource() == Voltar) {
+           
+                Window nova = new Window();
+                nova.CreateWindow();
+                
+           
         }
        
     }

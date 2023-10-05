4647982dbf4c26eb08eda66647a8fd1f0d5680c4
HenryAllenx <131827087+HenryAllenx@users.noreply.github.com>
2023-10-05 06:20:06 -0300

    Update Pedidos.java

--- Ecommerce--main/Ecommerce/demo/src/main/java/com/example/Window/Pedidos.java
+++ Ecommerce--main/Ecommerce/demo/src/main/java/com/example/Window/Pedidos.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
+import com.example.Json.Product;
+
 public class Pedidos implements ActionListener {
     final JFrame framePedidos = new JFrame("O que desejas fazer");
     JButton adicionarPedidos = new JButton("Fazer pedido");
@@ -36,18 +38,54 @@ public class Pedidos implements ActionListener {
         //------------------------------------//
 
         addPedidos.add(adicionarPedidos);
+        adicionarPedidos.addActionListener(this);
 
         verPedidos.add(pedidos);
+        pedidos.addActionListener(this);
 
         remover.add(remove);
+        remove.addActionListener(this);
 
         back.add(Voltar);
-
+        Voltar.addActionListener(this);
     }
      @Override
     public void actionPerformed(ActionEvent e) {
        if (e.getSource() == adicionarPedidos) {
+         final JFrame opcoes = new JFrame("Digite os detalhes do pedido");
+            opcoes.setLayout(null);
+            opcoes.setVisible(true);
+            opcoes.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            opcoes.setSize(600, 400);
+    
+        final JTextField nome = new JTextField("Digite o nome do produto");
+            nome.setBounds(10, 10, 200, 30);
+            opcoes.add(nome);
+            
+        final JTextField quantidade = new JTextField("Digite a quantidade de produto");
+            quantidade.setBounds(10, 90, 200, 30);
+            opcoes.add(quantidade);
+    
         
+
+    
+            JButton confirmar = new JButton("Confirmar");
+            confirmar.setBounds(10, 170, 100, 30);
+            opcoes.add(confirmar);
+
+            confirmar.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e){
+                    Product.adicionarProduto(null);
+
+                    opcoes.setVisible(false);
+                }
+            });
+       }
+       if (e.getSource() == Voltar) {
+            Window nova = new Window();
+            nova.CreateWindow();
+            
        }
     }
     
